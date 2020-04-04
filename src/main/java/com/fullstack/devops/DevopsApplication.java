adaebb52ed5235d98e4b96c5e4b2600822b41692
ananth1612 <36665835+ananth1612@users.noreply.github.com>
2020-04-04 17:29:12 +0200

    extends SpringBootServletInitializer added for tomcat deployment issue

--- src/main/java/com/fullstack/devops/DevopsApplication.java
+++ src/main/java/com/fullstack/devops/DevopsApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevopsApplication  {
+public class DevopsApplication  extends SpringBootServletInitializer {
 
 
-	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DevopsApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsApplication.class, args);
 	}
